450d9ab1c8f73daa76ff4e910f0a65694144757c
Pundalik1998 <pundalikmasurkar2001@gmail.com>
2025-12-29 19:41:12 +0530

    small change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Banner = () => {
   return (
     <div className='h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end' style={{backgroundImage: `url(https://cdn.marvel.com/content/2x/avengersendgame_lob_mas_mob_01.jpg)`}}>
-    <div className='text-white text-2xl text-center w-full bg-gray-900/60 p-4 '>Avengers Endgame</div>
+    <div className='text-white text-2xl text-center w-full bg-gray-900/60 p-4 '>Avengers Endgame 1</div>
  
     </div>
   )
